657083a6fab4529c345b54a713300df32176b393
Kishor Mainali <mainali1522kishor@gmail.com>
2021-02-26 12:11:14 +0545

    Plugins added

--- ios/Classes/SwiftFlutterSocialSharePlugin.swift
+++ ios/Classes/SwiftFlutterSocialSharePlugin.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-
 public class SwiftFlutterSocialSharePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_social_share", binaryMessenger: registrar.messenger())
